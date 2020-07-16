9a2d133df309d08f2277f164f2f00d99bccafdbc
nicholas DOOGE <ndooge@c3r7s4.wethinkcode.co.za>
2020-07-16 16:21:26 +0200

    00

---
+++ ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndooge <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 16:17:41 by ndooge            #+#    #+#             */
+/*   Updated: 2020/07/16 16:20:42 by ndooge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str1);
+void	ft_swap(int *a, int *b);
+#endif
